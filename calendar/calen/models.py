842b6fb32bb7a465c2ccf995b67b0f6d37c3c004
ps <peta.stadnik@email.cz>
2025-04-23 12:28:55 +0200

    23.4.2025

--- calendar/calen/models.py
+++ calendar/calen/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Udalost(models.Model):
+    jmeno = models.CharField(max_length=150, null=False)
+    popis = models.TextField(max_length=500, null=True)
+    def __str__(self):
+        return str(self.jmeno) + ": ("+ str(self.popis) + ")"
--- calendar/calen/views.py
+++ calendar/calen/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, "calen/index.html")
+    a = 5
+    b = a**2
+    c = 4
+    p = [13, "Liška", 999.99]
+    return render(request, "calen/index.html", {"a": a, "mocnina": b, "kriterium": c, "pole":p})
 
 def kolo(request):
     return HttpResponse("Stránka s koly!")
