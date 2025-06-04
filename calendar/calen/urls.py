289a8ca45d84384b61e434cf41b908b2d3a99cdc
ps <peta.stadnik@email.cz>
2025-06-04 12:15:37 +0200

    4.6.2025

--- calendar/calen/urls.py
+++ calendar/calen/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("kolo", views.kolo, name="kolo"),
     path("udalosti", views.udalosti, name="udalosti"),
-    path("detail/<int:udalost_id>", views.detail, name="detail")
+    path("detail/<int:udalost_id>", views.detail, name="detail"),
+    path("register", views.RegisterView.as_view(), name="register"),
+    path("logout", views.log_out, name="logout"),
+    path("login", views.log_in, name="login"),
 ]
--- calendar/calen/views.py
+++ calendar/calen/views.py
@@ -1,6 +1,11 @@
+import django.contrib.auth.forms as forms
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
+from django.views import generic
+from django.urls import reverse_lazy
 
 def index(request):
     a = 5
@@ -17,6 +22,32 @@ def udalosti(request):
     ud = Udalost.objects.all()
     return render(request, "calen/udalosti.html", {"udalosti": ud})
 
+@login_required
 def detail(request, udalost_id):
     ud = get_object_or_404(Udalost, pk=udalost_id)
     return render(request, "calen/detail_udalosti.html", {"udalost": ud})
+
+class RegisterView(generic.CreateView):
+    template_name = "calen/register.html"
+    success_url = reverse_lazy("calen:udalosti")
+    form_class = forms.UserCreationForm
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy("calen:udalosti"))
+
+def log_in(request):
+    form = forms.AuthenticationForm(request)
+    msg = ""
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse_lazy("calen:udalosti"))
+    elif request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            msg = "ŠPATNÉ JMÉNO NEBO HESLO"
+            return render(request, template_name="calen/login.html", context={'form': form, 'msg': msg})
+        else:
+            login(request, user)
+            return HttpResponseRedirect(reverse_lazy("calen:udalosti"))
+    else:
+        return render(request, 'calen/login.html', {'form':form, 'msg':msg})
